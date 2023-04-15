8ce59282e1f8825bc5ef37b1e336517da7a8fd9a
Evan <evanbell888@yahoo.com>
2023-04-15 17:19:34 +0000

    path

--- src/path_planning.py
+++ src/path_planning.py
@@ -2,10 +2,12 @@
 
 import rospy
 import numpy as np
+import tf
 import scipy.signal
+from heapdict import heapdict
 import math
-from geometry_msgs.msg import PoseStamped, PoseArray
-from nav_msgs.msg import Odometry, OccupancyGrid
+from geometry_msgs.msg import PoseStamped, PoseArray, Point
+from nav_msgs.msg import Odometry, OccupancyGrid, MapMetaData
 import rospkg
 import time, os
 from utils import LineTrajectory
@@ -34,6 +36,7 @@ class PathPlan(object):
         self.to_update_graph = False
         self.map = None
         self.lowres_map = None # downsampled version of map as an OccupancyGrid
+        self.stride = 1 #default of stride
         self.trajectory = LineTrajectory("/planned_trajectory")
         self.graph = None # as a set of vacant cells in lowres_map
         self.goal = None # as a PoseStamped in map
@@ -43,38 +46,43 @@ class PathPlan(object):
             self.main()
 
     def main(self):
-        if self.to_update_graph and self.map and self.goal and self.start:
+        if self.to_update_graph and self.graph and self.goal and self.start:
             self.to_update_graph = False
 
-            # create graph from map, start, and goal
-            self.graph = self.build_graph(self.lowres_map)
-
             # TODO (for Varied Resolution) convert start and goal to cell indices in graph of different resolution
+
             start = self.xy_to_uv(self.start.position.x, self.start.position.y)
             goal = self.xy_to_uv(self.goal.position.x, self.goal.position.y)
-
+         
+            
+            start = self.find_closest_node(start)[::-1]
+            goal = self.find_closest_node(goal)[::-1]
+            
             # plan path
-            #parent = self.find_shortest_path(self.graph, start, goal)
+            parent_path = self.find_shortest_path(self.graph, start, goal)
+
+            print(parent_path)
             # TODO (for Shortest Path Chain Reduction) collapse parent chain
-            #self.plan_path(self.graph, start, goal, parent)
+            if parent_path:
+                self.plan_path(parent_path)
 
 
     def map_cb(self, msg):
         """
         Callback for map. Updates the new map
         """
-        self.to_update_graph = True
         self.map = msg
+        print(msg.info)
         self.lowres_map = self.make_lowres_map(msg)
-        print('new map made', self.lowres_map.info)
-
+        self.graph = self.build_graph(self.lowres_map)
+        print('new map made')
+        print(self.lowres_map.info)
+        print(len(self.graph))
     def odom_cb(self, msg):
         """
         Callback for odometry. Updates the current pose to a cell index in the graph
         """
-        self.to_update_graph = True
-        self.start = msg.pose
-
+        self.start = msg.pose.pose
 
     def goal_cb(self, msg):
         """
@@ -82,20 +90,22 @@ class PathPlan(object):
         """
         self.to_update_graph = True
         self.goal = msg.pose
+        print('goal found')
 
-
-
-    def plan_path(self, graph, start, goal, parent):
+    def plan_path(self, parent_path):
         """
         Creates a LineTrajectory object and publishes it to the /trajectory/current topic.
         """
         # form PoseArray from parent values
         self.trajectory = LineTrajectory("/planned_trajectory")
-        p = goal
-        while p != start:
+        for (i,j) in parent_path:
+            u = (1.0*self.stride*(2*i+1)-1)/2
+            v = (1.0*self.stride*(2*j+1)-1)/2
+            
             # TODO (for Varied Resolution) convert p to a PoseStamped in map
-            self.trajectory.addPoint(self.uv_to_xy(p[0], p[1]))
-            p = parent[p]
+            p = Point()
+            p.x, p.y = self.uv_to_xy(v, u)
+            self.trajectory.addPoint(p)
 
         # publish trajectory
         self.traj_pub.publish(self.trajectory.toPoseArray())
@@ -125,10 +135,10 @@ class PathPlan(object):
                 0
                 ]
         
+        
         rotation_matrix = tf.transformations.quaternion_matrix(quaternion)
         a = np.array([u,v,0,0]).reshape(4,1)
         xy = np.matmul(rotation_matrix, a) + np.array(translation).reshape(4,1)
-
         return  xy.reshape(1,4)[0][:2]
 
 
@@ -150,13 +160,22 @@ class PathPlan(object):
                 self.map.info.origin.position.z,
                 0
                 ]
- 
+         
         a = np.array([x,y,0,0]).reshape(4,1)
         rotation_matrix = tf.transformations.quaternion_matrix(quaternion)
         b = np.linalg.inv(rotation_matrix)
         uv = np.matmul(b, a - np.array(translation).reshape(4,1) ) / self.map.info.resolution
         
         return uv.reshape(1,4)[0][:2]
+
+    def find_closest_node(self, (u,v)):
+        '''
+        finds the closest pixel node in the downsample version
+        '''
+        _u  = int(np.rint( ((2.0*u+1)/self.stride - 1)/2 ))
+        _v = int(np.rint( ((2.0*v+1)/self.stride - 1)/2 ))
+
+        return (_u, _v)
 
 
     def find_shortest_path(self, graph, start, goal):
@@ -167,60 +186,64 @@ class PathPlan(object):
         # TODO: UPDATE PARENT POINTERS
 
         #queue for holding nodes to look at, seen for seen nodes
-        queue = { (start, self. dist_between_points(start, goal)), }
-        seen = {start,}
-        parents = {start: None}  
-        
-        height  = graph.info.height
-        width  = graph.info.width
+        q = heapdict()
+        q[start] = self.dist_between_points(start, goal)
 
+        seen = {start,}
+        parents = {start: None} 
+
+        closest = (start, self.dist_between_points(start, goal))
+
+        
         #edge relation for each node
         neighbors = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (-1, -1), (1, -1), (-1, 1)]
     
-        while queue:
-
+        while q:
+            
             #pop off the queue, get node values i and j
-            node, _ = queue.pop()
+            node, _ = q.popitem()
+            if _ < closest[1]:
+                closest = (node, _)
             i,j = node
 
-            #base end, compute path back to start and return
-            if (i,j) == goal:
-                path = []
-                p = parents[(i,j)]
-                while p:
-                    path.append(p)
-                    p = parents[p]
-                return path[::-1]
+            if node == goal:
+                break
 
-
-            #for each neighbor, check if in bounds and not seen before, then add to seen. If not dark, add to the queue
+            #for each neighbor, check if not seen, then add to seen. If reachable, add to the queue
             for x,y in neighbors:
                 _x, _y = i+x, j+y
-                if height > _x  >=0 and width > _y >= 0 and (_x,_y) not in seen:
+                if (_x,_y) not in seen:
                     seen.add( (_x,_y) )
-                    if graph.data[_x][_y] < 30:
-                       queue.add( ((_x, _y), self.dist_between_points((_x,_y), goal)) )
-                       parents[(_x, _y)] = (x,y)
+                    if (_x,_y) in graph:
+                        q[(_x, _y)] =  self.dist_between_points((_x,_y), goal)
+                        parents[(_x, _y)] = (i,j)
 
             seen.add(node)
 
 
-        return None
+        #base end, compute path back to start and return
+        path = [closest[0]]
+        p = parents[closest[0]]
+        while p:
+            path.append(p)
+            p = parents[p]
+        return path[::-1]
 
     def dist_between_points(self, A, B):
         return ((1.0*A[0]-B[0])**2 + (1.0*A[1]-B[1])**2)**0.5
     
 
 
-    def make_lowres_map(self, map):
+    def make_lowres_map(self, _map):
         """
         Downsamples the map by a factor "stride".
         """
-        og_map_data = np.array(map.data).reshape(map.info.width, map.info.height)
+        og_map_data = np.array(_map.data).reshape(_map.info.width, _map.info.height)
 
         # params
-        filt = np.ones((7, 7)) # 0.05m x 3 = 15 cm buffer on each side
-        stride = 2
+        filt = np.ones((2,2)) # 0.05m x 3 = 15 cm buffer on each side
+        stride = 1
+        self.stride = stride
 
         # convolve
         stride_conv = lambda arr, arr2, s: scipy.signal.convolve2d(arr, arr2[::-1, ::-1], mode='valid')[::s, ::s]
@@ -228,22 +251,22 @@ class PathPlan(object):
 
         new_map = OccupancyGrid()
         new_map.data = new_map_data.flatten()
-        new_map.info = map.info
+        new_map.info = MapMetaData()
         new_map.info.width = new_map_data.shape[0]
         new_map.info.height = new_map_data.shape[1]
-        new_map.info.resolution = map.info.resolution * stride
+        new_map.info.resolution = _map.info.resolution * stride
 
         return new_map
     
 
-    def build_graph(self, map):
+    def build_graph(self, _map):
         """
         Builds a graph of vacant cells from the map data.
         """
         graph = set()
-        for i in range(map.info.height):
-            for j in range(map.info.width):
-                if map.data[i*map.info.width + j] == 0:
+        for i in range(_map.info.height):
+            for j in range(_map.info.width):
+                if _map.data[i*_map.info.width + j] == 0:
                     graph.add((i,j))
         return graph
   
@@ -257,7 +280,7 @@ class PathPlan(object):
         height  = OG.info.height
         width  = OG.info.width
 
-        graph = {}
+        graph = set()
 
 
         borders = set()
@@ -290,6 +313,7 @@ class PathPlan(object):
         #downsample graph by val
         meters_per_pixel_ideal = 0.2
         down_sample = int(np.rint(meters_per_pixel_ideal/OG.info.resolution))
+        self.stride = down_sample
         x = 0
         y = 0
         while x < height:
@@ -297,21 +321,20 @@ class PathPlan(object):
                 valid_node  = True
                 for i in range(x, x+down_sample):
                     for j in range(y, y+down_sample):
-                        if (i,j) in bad or (height > i  >=0 and width > j >= 0 and OG.data[i*width + j] != 0):
+                        if (i,j) in bad or (height > i  >= 0 and width > j >= 0 and OG.data[i*width + j] != 0):
                             valid_node  = False
                             break
                     if not valid_node:
                         break
                 if valid_node:
-                    _x = (1.0*down_sample*(2*x+1) - 1)/2 
-                    _y = (1.0*down_sample*(2*y+1) - 1)/2
-                    graph[(_x, _y)] = None
-                            
+                    _x = x/down_sample 
+                    _y = y/down_sample
+                    graph.add( (_x, _y) ) 
                 y += down_sample
             y = 0
             x  += down_sample
 
-        return graph, (height, width)
+        return graph
             
 if __name__=="__main__":
     rospy.init_node("path_planning")
