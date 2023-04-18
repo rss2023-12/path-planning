2a2d12ea781c8867c6d16b080310250a7288c5a6
cruzss <cruzss@mit.edu>
2023-04-18 23:33:09 +0000

    Some pure pursuit

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -16,9 +16,9 @@ class PurePursuit(object):
     """
     def __init__(self):
         self.odom_topic       = rospy.get_param("~odom_topic")
-        self.lookahead        = # FILL IN
-        self.speed            = # FILL IN
-        self.wheelbase_length = # FILL IN
+        self.lookahead        = 0.5
+        self.speed            = 10
+        self.wheelbase_length = 0.325
         self.trajectory  = utils.LineTrajectory("/followed_trajectory")
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
@@ -26,10 +26,21 @@ class PurePursuit(object):
     def trajectory_callback(self, msg):
         ''' Clears the currently followed trajectory, and loads the new one from the message
         '''
-        print "Receiving new trajectory:", len(msg.poses), "points"
+        print("Receiving new trajectory:", len(msg.poses), "points")
         self.trajectory.clear()
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
+        drive_cmd = AckermannDriveStamped()
+        L_1 = self.lookahead
+        L = self.wheelbase_length
+        rospy.logerr(self.trajectory.distances)
+        rospy.logerr(self.trajectory.points)
+        eta = 1 #needs to be set
+        drive_cmd.drive.steering_angle = -np.tan(2*L*np.sin(eta)/(L_1))
+        drive_cmd.drive.steering_angle_velocity = 1 #Needs to be set
+        drive_cmd.drive.speed = self.speed
+        
+        self.drive_pub.publish(drive_cmd)
 
 
 if __name__=="__main__":
--- src/trajectory_loader.py
+++ src/trajectory_loader.py
@@ -8,7 +8,7 @@ class LoadTrajectory(object):
     """ Loads a trajectory from the file system and publishes it to a ROS topic.
     """
     def __init__(self):
-        self.path           = rospy.get_param("~trajectory")
+        self.path           = r"/home/racecar/racecar_ws/src/lab6/trajectories/loop2.traj" #rospy.get_param("~trajectory")
 
         # initialize and load the trajectory
         self.trajectory = LineTrajectory("/loaded_trajectory")
